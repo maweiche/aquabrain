db85ac689cd2273b4801ee3c4b30ecb52da6f140
Matt Weichel <maweiche@gmail.com>
2023-04-15 13:30:49 -0400

    created next app, added logo

---
+++ src/pages/api/v1/Actuators.js
@@ -0,0 +1,23 @@
+// HTTP Method Relative URI Description
+// GET /api/v1/Actuators Get all actuators
+// POST /api/v1/Actuators Create a new actuator
+// PUT|PATCH /api/v1/Actuators/{Actuators}
+// Update an existing actuator identified by
+// {Actuators}
+// GET|HEAD /api/v1/Actuators/{Actuators} Get the actuator identified by {Actuators}
+// DELETE /api/v1/Actuators/{Actuators} Delete the actuator identified by {Actuators}
+// POST /api/v1/Command Create a new command
+// GET|HEAD /api/v1/Command Get all commands
+// GET|HEAD /api/v1/Command/{Command} Get command identified by {Command}
+// DELETE /api/v1/Command/{Command} Delete the command identified by {Command}
+// PUT|PATCH /api/v1/Command/{Command}
+// Update an existing command identified by
+// {Command}
+// POST /api/v1/Log Create new log data
+// GET|HEAD /api/v1/SensorLog/{SensorLog} Get sensor log identified by {SensorLog}
+// GET|HEAD /api/v1/SensorTypes Get all sensor types
+// GET|HEAD /api/v1/SensorTypes/{SensorTypes} Get sensor type identified by {SensorType}
+// GET|HEAD /api/v1/Sensors Get all Sensors
+// GET|HEAD /api/v1/Sensors/{Sensors} Get sensor identified by {Sensors}
+// DELETE /api/v1/Sensors/{Sensors} Delete sensor identified by {Sensors}
+// PUT|PATCH /api/v1/Sensors/{Sensors} Update existing sensor identified by {Sensors}
